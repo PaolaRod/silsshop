4ee3b17d6f72b9594178235c9273f208a5aa8201
Paola Rod <pao.alejandra1992@gmail.com>
2024-07-28 16:03:57 -0300

    Se agrega el readme, y se eliminan funciones que no se utilizan

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# silsshop
+# SilsShop - Automation Tests
+
+> "Testing leads to failure, and failure leads to understanding." @burtrutan
+
+This project provides some automated tests, based on [Cypress](https://www.cypress.io/),
+to the e-commerce: [silsshop.sils.tech](https://silsshop.sils.tech).
+
+## Installation
+
+1. Clone this project: `git clone git@github.com:PaolaRod/silsshop.git`
+2. Install dependencies: `npm ci`
+
+## Run
+
+1. Open the Cypress app: `npx cypress open`
+2. In the Cypress page, select the option _"E2E Testing"_
+3. Select your preferred browser to run the automated tests
+4. Press the _"Start E2E Testing"_ button
+5. Select the automated test set you want to run
+
+## Tests description
+
+The automated cases are:
+
+1. Add a random Item to the Shopping Cart successfully
+2. Remove an Item from the Shopping Cart successfully
--- cypress/support/pages/Carrito.Page.js
+++ cypress/support/pages/Carrito.Page.js
@@ -4,13 +4,11 @@ class ShoppingCart {
     productCard: () => cy.get('div.product-item-img'),
     productName: () => cy.get('div.product-single .title'),
     productPrice: () => cy.get('div.product-single .price .new-price'),
-    //shoppingCartButton: () => cy.get('button.add-to-cart-btn'),
     addToCartButton: () => cy.get('button.add-to-cart-btn'),
     shoppingCartIconNumber: () => cy.get('.cart-btn .cart-items-value'),
     shoppingCartButton: () => cy.get('.cart-btn'),
     shoppingCartItem: () => cy.get('div.cart-ctr.py-4>').find('span.cart-ctxt'),
-    deleteButton: () => cy.get('button.delete-btn').eq(0),
-    emptyCartText: () => cy.get('.empty-cart>.text-gray')
+    deleteButton: () => cy.get('button.delete-btn').eq(0)
   };
 
   selectRandomCategorie() {
@@ -39,10 +37,6 @@ class ShoppingCart {
         const productPrice = text.trim();
         cy.wrap(productPrice).as('productPrice');
     });
-  }
-
-  ClickProductCard() {
-    this.get.productCard().click();
   }
 
   GoToShoppingCart() {
